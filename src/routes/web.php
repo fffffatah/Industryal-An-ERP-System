ed7430f6bd59b471db92705722e13d592fe2b1d8
Atanusaha143 <atanu.saha415@gmail.com>
2021-06-18 04:31:59 +0600

    added product home route

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductHomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Product
+Route::get('/product/home',[ProductHomeController::class, 'index'])->name('producthome.index');
