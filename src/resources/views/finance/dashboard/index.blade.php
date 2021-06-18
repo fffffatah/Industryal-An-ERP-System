3c08f630087a61b06c3258c01dcf4f8ac986973a
fffffatah <ab.fatahmn@gmail.com>
2021-06-18 22:54:19 +0600

    divide all routes

--- src/resources/views/finance/dashboard/index.blade.php
+++ src/resources/views/finance/dashboard/index.blade.php
@@ -1,3 +1,125 @@
 @include('finance.layouts.header');
-<!--CONTENTS-->
+<div style="padding-top:100px;">
+<center>
+    <table>
+        <tr>
+            <td align="left" style="padding:20px;">
+            <div class="card border-warning mb3" style="height:300px;width:1000px">
+                <div class="card-header">Organization</div>
+                    <div class="card-body">
+                    <table>
+                            <tr>
+                                <th>Name: </th>
+                                <td>abc</td>
+                            </tr>
+                            <tr>
+                                <th>Email: </th>
+                                <td>abc@def.com</td>
+                            </tr>
+                            <tr>
+                                <th>Phone: </th>
+                                <td>1234567</td>
+                            </tr>
+                            <tr>
+                                <th>EST: </th>
+                                <td>12/12/1999</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </td>
+            <td align="center" style="padding:20px;">
+            </td>
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <td align="center" style="padding:20px;">
+            <div class="card border-warning mb3" style="height:300px;width:250px">
+                <div class="card-header">Product</div>
+                    <div class="card-body">
+                        <table>
+                            <tr>
+                                <th>Manager: </th>
+                                <td>John Doe</td>
+                            </tr>
+                            <tr>
+                                <th>Email: </th>
+                                <td>abc@def.com</td>
+                            </tr>
+                            <tr>
+                                <th>Phone: </th>
+                                <td>1234567</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </td>
+            <td align="center" style="padding:20px;">
+            <div class="card border-info mb3" style="height:300px;width:250px">
+                <div class="card-header">Sales & Marketing</div>
+                    <div class="card-body">
+                    <table>
+                            <tr>
+                                <th>Manager: </th>
+                                <td>John Doe</td>
+                            </tr>
+                            <tr>
+                                <th>Email: </th>
+                                <td>abc@def.com</td>
+                            </tr>
+                            <tr>
+                                <th>Phone: </th>
+                                <td>1234567</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </td>
+            <td align="center" style="padding:20px;">
+            <div class="card border-success mb3" style="height:300px;width:250px">
+                <div class="card-header">HR</div>
+                    <div class="card-body">
+                    <table>
+                            <tr>
+                                <th>Manager: </th>
+                                <td>John Doe</td>
+                            </tr>
+                            <tr>
+                                <th>Email: </th>
+                                <td>abc@def.com</td>
+                            </tr>
+                            <tr>
+                                <th>Phone: </th>
+                                <td>1234567</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </td>
+            <td align="center" style="padding:20px;">
+            <div class="card border-danger mb3" style="height:300px;width:250px">
+                <div class="card-header">Finance</div>
+                    <div class="card-body">
+                    <table>
+                            <tr>
+                                <th>Manager: </th>
+                                <td>John Doe</td>
+                            </tr>
+                            <tr>
+                                <th>Email: </th>
+                                <td>abc@def.com</td>
+                            </tr>
+                            <tr>
+                                <th>Phone: </th>
+                                <td>1234567</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </td>
+        </tr>
+    </table>
+</center>
+</div>
 @include('finance.layouts.footer');
