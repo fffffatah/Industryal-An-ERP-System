71510b0fd25233d1ae02e703bda03574030d01a2
fffffatah <ab.fatahmn@gmail.com>
2021-06-20 19:38:34 +0600

    complete finance import export feature

--- src/resources/views/finance/importexport/export.blade.php
+++ src/resources/views/finance/importexport/export.blade.php
@@ -11,7 +11,27 @@
             <div class="card border-warning mb3" style="height:600px;width:1100px">
                 <div class="card-header">Export</div>
                     <div class="card-body">
-                        <h1 align="center" style="color:orange; font-size:80px;">Select from Options</h1>
+                        <h1 align="center" style="color:green; font-size:20px;">Select data that you want to export</h1><br><br>
+                        <table>
+                            <tr>
+                                <th><input type="checkbox" id="invoiceexport" name="invoiceexport" value=""><label for="invoiceexport">  Invoice</label></th>
+                            </tr>
+                            <tr>
+                                <th><input type="checkbox" id="paymentexport" name="paymentexport" value=""><label for="paymentexport"> Payments</label></th>
+                            </tr>
+                            <tr>
+                                <th><input type="checkbox" id="reporthistoryexport" name="reporthistoryexport" value=""><label for="reporthistoryexport"> Report Generation History</label></th>
+                            </tr>
+                            <tr>
+                                <th><input type="checkbox" id="budgetingexport" name="budgetingexport" value=""><label for="budgetingexport"> Budgeting</label></th>
+                            </tr>
+                            <tr>
+                                <th><input type="checkbox" id="myleavereqexport" name="myleavereqexport" value=""><label for="myleavereqexport"> My Leave-Request</label></th>
+                            </tr>
+                            <tr>
+                                <th><input type="checkbox" id="historyexport" name="historyexport" value=""><label for="historyexport"> Import/Export History</label></th>
+                            </tr>
+                        </table>
                     </div>
                 <div class="card-footer"><a class="btn btn-info text-left" href="#"><i class="fas fa-download"></i> Export as CSV</a> <a class="btn btn-info text-left" href="#"><i class="fas fa-download"></i> Export as XML</a></div>
                 </div>
