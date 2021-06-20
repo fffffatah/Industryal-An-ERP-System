e1f377a6f3c43e364ba9bb62ffc66a8523b50246
fffffatah <ab.fatahmn@gmail.com>
2021-06-20 21:25:40 +0600

    add views to finance payment feature

---
+++ src/resources/views/finance/layouts/paymentoptions.blade.php
@@ -0,0 +1,11 @@
+<div class="card border-warning mb3" style="height:600px;width:250px">
+                <div class="card-header">Payment Options</div>
+                    <div class="card-body">
+                        <table align="left">
+                        <div class="btn-group-vertical">
+                            <a class="btn btn-info text-left" href="#"><i class="fas fa-list"></i> Payment History</a>
+                            <a class="btn btn-info text-left" href="#"><i class="fas fa-exclamation"></i> Pending</a>
+                        </div>
+                        </table>
+                    </div>
+                </div>
