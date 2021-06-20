bf2481e5f73a8cb91be6a91b803ff99eb935c3f0
fffffatah <ab.fatahmn@gmail.com>
2021-06-20 20:07:22 +0600

    complete finance leave request feature

--- src/resources/views/finance/leaverequest/list.blade.php
+++ src/resources/views/finance/leaverequest/list.blade.php
@@ -10,8 +10,23 @@
             <td align="center">
             <div class="card border-warning mb3" style="height:600px;width:1100px">
                 <div class="card-header">My Leave-Requests</div>
-                    <div class="card-body">
-                        <h1 align="center" style="color:orange; font-size:80px;">Select from Options</h1>
+                <div class="card-body scroll-box">
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <tr>
+                                <th scope="col">#SR</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">Title</th>
+                                <th scope="col">Status</th>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>12/12/2047</td>
+                                <td>abcd</td>
+                                <td>Approved</td>
+                            </tr>
+                        </table>
+                    </div>
                     </div>
             </div>
             </td>
