c5a9e8abf3dfddb296218a30029e2eda561b98e5
Atanusaha143 <atanu.saha415@gmail.com>
2021-06-27 01:13:15 +0600

    added warehouse edit functionality

--- src/app/Http/Controllers/WarehouseListController.php
+++ src/app/Http/Controllers/WarehouseListController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product\warehouse_table;
 use App\Models\Product\product_table;
+use App\Http\Requests\Product\WarehouseCreateRequest;
 
 class WarehouseListController extends Controller
 {
@@ -15,5 +16,29 @@ class WarehouseListController extends Controller
         $product = product_table::all();
 
         return view('product.warehouse.list.index')->with('warehouseList', $warehouse_list)->with('productList', $product);
+    }
+
+    public function editWarehouse($warehouse_id)
+    {
+        $warehouse = warehouse_table::where('warehouse_id', $warehouse_id)->first();
+        return view('product.warehouse.list.edit')->with('warehouse', $warehouse);
+    }
+
+    public function updateWarehouse(WarehouseCreateRequest $req, $warehouse_id)
+    {
+        $warehouse = warehouse_table::where('warehouse_id', $warehouse_id)->first();
+        $warehouse->warehouse_id = $req->warehouse_id;
+        $warehouse->name = $req->warehouse_name;
+        $warehouse->description = $req->warehouse_description;
+        $warehouse->address = $req->warehouse_address;
+        $warehouse->zip_code = $req->warehouse_zip_code;
+        $warehouse->city = $req->warehouse_city;
+        $warehouse->country = $req->warehouse_country;
+        $warehouse->phone = $req->warehouse_phone;
+        $warehouse->status = $req->warehouse_status;
+        $warehouse->quantity = $req->warehouse_quantity;
+        $warehouse->save();
+
+        return redirect()->route('warehouseList.index');
     }
 }
