a295906db3935182c092b8d6ecf54d5690cf223c
Atanusaha143 <atanu.saha415@gmail.com>
2021-06-26 23:14:14 +0600

    added back-end for warehouse list

--- src/app/Http/Controllers/WarehouseListController.php
+++ src/app/Http/Controllers/WarehouseListController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product\warehouse_table;
 
 class WarehouseListController extends Controller
 {
     public function index()
     {
-        return view('product.warehouse.list.index');
+        $warehouse_list = warehouse_table::all();
+        return view('product.warehouse.list.index')->with('warehouseList', $warehouse_list);
     }
 }
