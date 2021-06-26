62d5e1e0f25bfdaa950ded0066757cdf9e081a17
Atanusaha143 <atanu.saha415@gmail.com>
2021-06-26 18:39:25 +0600

    added delete funtionality for product

--- src/app/Http/Controllers/ProductListController.php
+++ src/app/Http/Controllers/ProductListController.php
@@ -14,6 +14,18 @@ class ProductListController extends Controller
         return view('product.list.index')->with('productList', $list);
     }
 
+    public function deleteProduct($product_id)
+    {
+        $product = product_table::where('product_id', $product_id)->first();
+        return view('product.list.details')->with('product', $product);
+    }
+
+    public function destroyProduct($product_id)
+    {
+        product_table::where('product_id', $product_id)->delete();
+        return redirect()->route('productList.index');
+    }
+
     public function faulty()
     {
         return view('product.list.faulty');
