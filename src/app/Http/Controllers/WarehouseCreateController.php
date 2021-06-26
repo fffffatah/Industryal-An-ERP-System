8f094d48e29eee7f49ec163646e705bbab5285a5
Atanusaha143 <atanu.saha415@gmail.com>
2021-06-26 23:13:37 +0600

    added back-end for warehouse creation

--- src/app/Http/Controllers/WarehouseCreateController.php
+++ src/app/Http/Controllers/WarehouseCreateController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\Product\WarehouseCreateRequest;
+use App\Models\Product\warehouse_table;
 
 class WarehouseCreateController extends Controller
 {
@@ -15,6 +16,20 @@ class WarehouseCreateController extends Controller
 
     public function create(WarehouseCreateRequest $req)
     {
+        $warehouse = new warehouse_table;
+
+        $warehouse->warehouse_id = $req->warehouse_id;
+        $warehouse->name = $req->warehouse_name;
+        $warehouse->description = $req->warehouse_description;
+        $warehouse->address = $req->warehouse_address;
+        $warehouse->zip_code = $req->warehouse_zip_code;
+        $warehouse->city = $req->warehouse_city;
+        $warehouse->country = $req->warehouse_country;
+        $warehouse->phone = $req->warehouse_phone;
+        $warehouse->status = $req->warehouse_status;
+        $warehouse->quantity = $req->warehouse_quantity;
+        $warehouse->save();
+
         return redirect()->route('warehouseList.index');
     }
 }
