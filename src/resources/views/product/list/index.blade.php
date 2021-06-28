c1b12497dbb8363f91c8c340d8efb218dc53841a
Atanusaha143 <atanu.saha415@gmail.com>
2021-06-29 00:44:07 +0600

    updated front-end for product list

--- src/resources/views/product/list/index.blade.php
+++ src/resources/views/product/list/index.blade.php
@@ -116,7 +116,7 @@
                                         </td>
                                         <td>{{$product['product_condition']}}</td>
                                         <td>
-                                        <a href="/product/edit/{{$product['product_id']}}" class="btn btn-success mb-2">Update</a>
+                                        <a href="/product/edit/{{$product['product_id']}}" class="btn btn-warning mb-2">Update</a>
                                         <a href="/product/delete/{{$product['product_id']}}" class="btn btn-danger">Delete</a>
                                         </td>
                                     </tr>
