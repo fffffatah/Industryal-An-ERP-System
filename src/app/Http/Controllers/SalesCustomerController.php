12ec73f028fa12103f32882fdfa768f35aa2321e
snigdho611 <snigdho.howlader@gmail.com>
2021-06-28 12:42:38 +0600

    Updating controller for customer

--- src/app/Http/Controllers/SalesCustomerController.php
+++ src/app/Http/Controllers/SalesCustomerController.php
@@ -13,6 +13,23 @@ class SalesCustomerController extends Controller
 
     public function sendEmail()
     {
-        return view('sales.customers.send');
+        return view('sales.customers.mail.send');
+    }
+
+    public function allMail()
+    {
+        return view('sales.customers.mail.all');
+    }
+    public function sentMail()
+    {
+        return view('sales.customers.mail.sent');
+    }
+    public function receivedMail()
+    {
+        return view('sales.customers.mail.received');
+    }
+    public function spamMail()
+    {
+        return view('sales.customers.mail.spam');
     }
 }
