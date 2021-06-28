146523c8fc3f3f9626a329076a716c24e40dc5e4
snigdho611 <snigdho.howlader@gmail.com>
2021-06-28 12:43:10 +0600

    Fixing customer list files at sales

--- src/resources/views/sales/customers/list.blade.php
+++ src/resources/views/sales/customers/list.blade.php
@@ -9,30 +9,17 @@
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Tony Stark</td>
-        <td>tony.stark@gmail.com</td>
-        <td>+1 NXX-NXX-XXXX</td>
-        <td align="center"><button type="button" class="btn btn-info" onclick="document.location='{{route('sales.customers.send')}}'">Mail Customer</button>
-
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Bruce Wayne</td>
-        <td>batman@gmail.com</td>
-        <td>+1 NXX-NXX-XXXX</td>
-        <td align="center"><button type="button" class="btn btn-info" onclick="document.location='{{route('sales.customers.send')}}'">Mail Customer</button>
-
-      </tr>
+      @for($i=0;$i<3;$i++)
       <tr>
         <th scope="row">3</th>
         <td scope="row">Peter Parker</td>
         <td>new.yorker@gmail.com</td>
         <td>+1 NXX-NXX-XXXX</td>
-        <td align="center"><button type="button" class="btn btn-info" onclick="document.location='{{route('sales.customers.send')}}'">Mail Customer</button>
+        <td align="center">
+          <a class="btn btn-info text-left" href="{{route('sales.customers.mail.send')}}">Mail Customer</a>
         </td>
       </tr>
+      @endfor
     </tbody>
   </table>
 @include('sales.layouts.footer')
