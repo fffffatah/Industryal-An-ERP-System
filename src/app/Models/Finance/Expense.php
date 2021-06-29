615936f6eaceb5dffbbcf7db2ea4b035cd99fd91
fffffatah <ab.fatahmn@gmail.com>
2021-06-29 22:11:47 +0600

    add timestamps to model finance

--- src/app/Models/Finance/Expense.php
+++ src/app/Models/Finance/Expense.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
-    public $timestamps=false;
+    public $timestamps=true;
 }
--- src/app/Models/Finance/Liability.php
+++ src/app/Models/Finance/Liability.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Liability extends Model
 {
-    public $timestamps=false;
+    public $timestamps=true;
 }
