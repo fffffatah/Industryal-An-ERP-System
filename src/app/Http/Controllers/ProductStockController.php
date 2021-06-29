66be51cd5e6d9834f07a0fc5855f4262b5825e75
Atanusaha143 <atanu.saha415@gmail.com>
2021-06-30 00:37:07 +0600

    added search functionality for product stock controller

--- src/app/Http/Controllers/ProductStockController.php
+++ src/app/Http/Controllers/ProductStockController.php
@@ -8,9 +8,17 @@ use App\Models\Product\product_table;
 
 class ProductStockController extends Controller
 {
-    public function index()
+    public function index(Request $req)
     {
-        $productList = product_table::all();
-        return view('product.stocks.index')->with('productList', $productList);
+        if($req->searchProduct)
+        {
+            $searchProduct = product_table::where('product_name', $req->searchProduct)->get();
+            return view('product.stocks.index')->with('productList', $searchProduct);
+        }
+        else
+        {
+            $productList = product_table::all();
+            return view('product.stocks.index')->with('productList', $productList);
+        }
     }
 }
