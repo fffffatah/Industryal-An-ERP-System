77770c12888eb92e55d6d0951264290d2f4fae3a
Atanusaha143 <atanu.saha415@gmail.com>
2021-06-30 01:21:50 +0600

    updated front-end to for page title

--- src/resources/views/product/user/activities/index.blade.php
+++ src/resources/views/product/user/activities/index.blade.php
@@ -10,7 +10,7 @@
     crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous"> <!-- for fontawesome -->
 
-    <title>Product | Home</title>
+    <title>User | Activities</title>
 </head>
 <body>
     <!-- Header Starts -->
