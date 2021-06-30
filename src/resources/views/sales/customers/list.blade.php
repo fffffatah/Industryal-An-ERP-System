0b5e5eb9b0477da5843a29da6cf97b97a5877129
snigdho611 <snigdho.howlader@gmail.com>
2021-06-30 07:30:48 +0600

    Fixing urls for the page: customer list

--- src/resources/views/sales/customers/list.blade.php
+++ src/resources/views/sales/customers/list.blade.php
@@ -18,10 +18,10 @@
         <td>{{$customer->phone}}</td>
         <td>{{$customer->first_purchase}}</td>
         <td align="center">
-          <a class="btn btn-info text-left" href="./mail/send/{{$customer['id']}}">Mail Customer</a>
+          <a class="btn btn-info text-left" href="../mail/send/{{$customer['id']}}">Mail Customer</a>
         </td>
         <td align="center">
-          <a class="btn btn-info text-left" href="./customers/update/{{$customer['id']}}">Update</a>
+          <a class="btn btn-info text-left" href="./update/{{$customer['id']}}">Update</a>
         </td>
       </tr>
       @endforeach
