5f7e9c47733376f002b093d2cfa1e928ba9ec3c1
snigdho611 <snigdho.howlader@gmail.com>
2021-07-01 19:33:45 +0600

    Create confirm.blade.php

---
+++ src/resources/views/sales/orders/transactions/confirm.blade.php
@@ -0,0 +1,12 @@
+@include('sales.layouts.header')
+<table>
+  <tr>
+    <td style="width:800px;">
+      
+    </td>
+    <td>
+      abc
+    </td>
+  </tr>
+</table>
+@include('sales.layouts.footer')
