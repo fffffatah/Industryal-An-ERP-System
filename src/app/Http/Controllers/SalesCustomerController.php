b23cfee4aa663dfe6fa470de29835b78bc23d0c7
snigdho611 <snigdho.howlader@gmail.com>
2021-07-01 12:27:09 +0600

    Update SalesCustomerController.php

--- src/app/Http/Controllers/SalesCustomerController.php
+++ src/app/Http/Controllers/SalesCustomerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use App\Http\Requests\Sales\SalesCustomerUpdateRequest;
+use App\Http\Requests\Sales\SalesCustomerInsertRequest;
 use Illuminate\Http\Request;
 use App\Models\Sales\CustomerModel;
 
@@ -41,47 +42,24 @@ class SalesCustomerController extends Controller
 
     public function createCustomer() 
     {
-        // $customers = CustomerModel::all();
-        $lastCustomer = DB::table('customers')->latest('id')->first();
-        // $customers->save();
-        // $id = $customers->id;
-        return view('sales.customers.create')->with('id', strval(intval($lastCustomer->id)+1));
+        // $lastCustomer = DB::table('customers')->latest('id')->first();
+        $statement = DB::select("SHOW TABLE STATUS LIKE 'customers'");
+        $nextId = $statement[0]->Auto_increment;
+        return view('sales.customers.create')->with('id', $nextId);
     }
 
-    // public function updateProduct(ProductCreateRequest $req,$id)
-    // {
-    //      $product = product_table::where('id', $id)->first();
-    //      $product->product_id = $req->product_id;
-    //      $product->product_name = $req->product_name;
-    //      $product->status_sell = $req->product_sell_status;
-    //      $product->status_purchase = $req->product_purchase_status;
-    //      $product->product_description = $req->product_description;
-    //      $product->warehouse_name = $req->warehouse_name;
-    //      $product->stock = $req->product_stock;
-    //      $product->nature = $req->product_nature;
-    //      $product->weight = $req->product_weight;
-    //      $product->weight_unit = $req->product_weight_unit;
-    //      $product->dimention = $req->product_dimention;
-    //      $product->dimention_unit = $req->product_dimention_unit;
-    //      $product->selling_price = $req->product_selling_price;
-    //      $product->tax = $req->product_selling_tax;
-    //      $product->product_condition = $req->product_condition;
-    //      $product->last_updated = date('Y-m-d');
-    //      $product->save();
-
-    //      // activity
-    //     $activity = new activities_table;
-    //     $activity->type = "Update Product";
-    //     $activity->description = "Product Id: ".$req->product_id.", "."Product Name: ".$req->product_name;
-    //     $activity->activity_time = date("Y-m-d H:i:s");
-    //     $activity->save();
-
-    //     return redirect()->route('productList.index');
-    // }
-    // public function editProduct($id)
-    // {
-    //     $product = product_table::where('id', $id)->first();
-    //     $warehouseList = warehouse_table::pluck('name');
-    //     return view('product.list.edit')->with('product', $product)->with('warehouseList',$warehouseList);
-    // }
+    public function insertCustomer(SalesCustomerInsertRequest $req) 
+    {
+        $customer = new CustomerModel;
+        $customer->name = $req->cus_name;
+        $customer->email = $req->cus_email;
+        $customer->phone = $req->cus_phone;
+        $customer->type = "regular";
+        $customer->delivery_point = $req->cus_del;
+        $customer->updated_at = date('Y-m-d');
+        $customer->first_purchase = date('Y-m-d');
+        $customer->save();
+        $req->session()->flash('successful', 'Successfully updated!');
+        return redirect()->route('sales.customers.list');
+    }
 }
