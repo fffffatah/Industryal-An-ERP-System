68bd6200133ece4bbd509ab3dd0299fb0b070ed3
snigdho611 <snigdho.howlader@gmail.com>
2021-07-03 01:20:10 +0600

    Added ajax search engine for orders

--- src/app/Http/Controllers/SalesSearchController.php
+++ src/app/Http/Controllers/SalesSearchController.php
@@ -19,15 +19,29 @@ class SalesSearchController extends Controller
         {
             $output="";
             $orders=DB::table('orders')->where('id','LIKE','%'.$request->search."%")->get();
-            if($orders)
+            if(!($orders->isEmpty()))
             {
                 foreach ($orders as $key => $order) {
                     $output.='<tr>'.
-                    '<td>'.$order->id.'</td>'.
-                    '<td>'.$order->order_description.'</td>'.
+                        '<th>'.$order->id.'</th>'.
+                        '<td>'.$order->customer_id.'</td>'.
+                        '<td>'.$order->order_description.'</td>'.
+                        '<td>'.$order->total_amount.'</td>'.
+                        '<td>'.$order->order_made.'</td>'.
+                        '<td>'.$order->status.'</td>'.
+                        '<td>'.$order->delivered_on.'</td>'.
+                        '<td align="center">
+                            <a class="btn btn-info text-left" href="#">Update</a>
+                        </td>'.
                     '</tr>';
                 }
                 return Response($output);
+            }
+            elseif($orders->isEmpty())
+            {
+                return Response("<div style='margin:auto;width:100%;color:rgb(172, 4, 4);'>
+                <h4>Looks like the id you entered does not exist!</h4>
+              </div>");
             }
         }
     }
