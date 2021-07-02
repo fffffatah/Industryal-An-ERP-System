4a6368dbb300a79525f48653ce7c2ca23dea6318
Rasel <rasel.aiub990@gmail.com>
2021-07-03 05:07:46 +0600

    admin dashbord

--- src/app/Http/Controllers/AdminDashboardController.php
+++ src/app/Http/Controllers/AdminDashboardController.php
@@ -8,7 +8,9 @@ use App\Models\User;
 class AdminDashboardController extends Controller
 {
     public function index(){
-        $HrManger=User::where('type','hr')->first();
-        return view('common.admin.index')->with('HrManager',$HrManger);
+
+        $productManager = User::where('type','product')->first();
+        return view('common.admin.index')->with('productManegerDetails', $productManager);
+        
     }
 }
