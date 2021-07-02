4db00e5748fb86973b2d422a8f7e1a1789ccc6e0
Atanusaha143 <atanu.saha415@gmail.com>
2021-07-02 13:53:39 +0600

    added route to search product

--- src/routes/erp/product/web.php
+++ src/routes/erp/product/web.php
@@ -29,6 +29,7 @@ Route::get('/', function () {
 Route::get('/',[ProductUserController::class, 'logout'])->name('user.logout');
 
 Route::group(["middleware" => "peroduct_session"],function () {
+    
                                     // Products
 //Product
 Route::get('/product/home',[ProductHomeController::class, 'index'])->name('productHome.index');
@@ -36,62 +37,83 @@ Route::get('/product/home',[ProductHomeController::class, 'index'])->name('produ
 Route::get('/product/create',[ProductCreateController::class, 'index'])->name('productCreate.index');
 Route::post('/product/create',[ProductCreateController::class, 'create']);
 
+// list of good products
 Route::get('/product/list',[ProductListController::class, 'index'])->name('productList.index');
 Route::post('/product/list',[ProductListController::class, 'index']);
 
+// list of faulty products
 Route::get('/product/list/faulty',[ProductListController::class, 'faulty'])->name('productListFaulty.index');
 
-// Update
+// autocomplete search good product
+Route::post('/product/list/search',[ProductListController::class, 'search'])->name('productSearch.search');
+
+// Update Product
 Route::get('/product/edit/{id}',[ProductListController::class, 'editProduct'])->name('productList.editProduct');
 Route::post('/product/edit/{id}',[ProductListController::class, 'updateProduct']);
 
+// delete product
 Route::get('/product/delete/{id}',[ProductListController::class, 'deleteProduct'])->name('productList.deleteProduct');
 Route::post('/product/delete/{id}',[ProductListController::class, 'destroyProduct']);
 
+// stocked product
 Route::get('/product/stocks',[ProductStockController::class, 'index'])->name('productStocks.index');
 Route::post('/product/stocks',[ProductStockController::class, 'index']);
 
+// product statistics
 Route::get('/product/statistics',[ProductStatisticsController::class, 'index'])->name('productStatistics.index');
 
+// transfer product
 Route::get('/product/transfer',[ProductTransferController::class, 'index'])->name('productTransfer.index');
 Route::post('/product/transfer',[ProductTransferController::class, 'transfer']);
 
-//Warehouse
+
+                                //Warehouse
+// create warehouse
 Route::get('/warehouse/create',[WarehouseCreateController::class, 'index'])->name('warehouseCreate.index');
 Route::post('/warehouse/create',[WarehouseCreateController::class, 'create']);
 
+// list of warehouse
 Route::get('/warehouse/list',[WarehouseListController::class, 'index'])->name('warehouseList.index');
 Route::post('/warehouse/list',[WarehouseListController::class, 'index']);
 
-// Edit
+// Edit warehouse
 Route::get('/warehouse/edit/{id}',[WarehouseListController::class, 'editWarehouse'])->name('warehouseList.edit');
 Route::post('/warehouse/edit/{id}',[WarehouseListController::class, 'updateWarehouse']);
 
+// Warehouse statistics
 Route::get('/warehouse/statistics',[WarehouseStatisticsController::class, 'index'])->name('warehouseStatistics.index');
 
-//Others
+
+                                    //Others
+// user activities
 Route::get('/product/user/activities',[ProductUserController::class, 'activities'])->name('userActivities.index');
 Route::post('/product/user/activities',[ProductUserController::class, 'activities']);
 
+// user levae
 Route::get('/product/user/leave',[ProductUserController::class, 'leave'])->name('userLeave.index');
 Route::post('/product/user/leave',[ProductUserController::class, 'verifyLeave']);
 Route::get('/product/user/leave/myrequest',[ProductUserController::class, 'myLeave'])->name('myLeave.index');
 
+// contact administration
 Route::get('/product/user/administration',[ProductUserController::class, 'administration'])->name('userAdministration.index');
 Route::post('/product/user/administration',[ProductUserController::class, 'verifyAdministration']);
 
-//User
+//User profile
 Route::get('/product/user/profile',[ProductUserController::class, 'profile'])->name('userProfile.index');
 
+// user edit profile
 Route::get('/product/user/edit',[ProductUserController::class, 'editProfile'])->name('userEditProfile.index');
 Route::post('/product/user/edit',[ProductUserController::class, 'editProfileVerify']);
 
+// user edit profile picture
 Route::get('/product/user/edit/profilepicture',[ProductUserController::class, 'editProfilePicture'])->name('userEditProfilePicture.index');
 Route::post('/product/user/edit/profilepicture',[ProductUserController::class, 'editProfilePictureVerify']);
 
+// user change password
 Route::get('/product/user/edit/changePassword',[ProductUserController::class, 'changePassword'])->name('userChangePassword.index');
 Route::post('/product/user/edit/changePassword',[ProductUserController::class, 'changePasswordVerify']);
 
+// user change password verification code
 Route::get('/product/user/edit/changePassword/verify',[ProductUserController::class, 'verification'])->name('userChangeProfileVerication.index');
 Route::post('/product/user/edit/changePassword/verify',[ProductUserController::class, 'verificationVerify']);
 
