f489be27827095a2983a4d8b9d04d32f547769f3
snigdho611 <snigdho.howlader@gmail.com>
2021-08-09 04:45:26 +0600

    rollback: failed implementing sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,30 +3,7 @@ import SideMenu from './components/SideMenu';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 import './App.css'
 
-const sidebarBox = document.querySelector('#Menu'),
-        sidebarBtn = document.querySelector('#btn'),
-        pageWrapper = document.querySelector('#sideMenu');
 
-sidebarBtn.addEventListener('click', event => {
-		sidebarBtn.classList.toggle('active');
-		sidebarBox.classList.toggle('active');
-});
-
-pageWrapper.addEventListener('click', event => {
-
-		if (sidebarBox.classList.contains('active')) {
-            sidebarBtn.classList.remove('active');
-            sidebarBox.classList.remove('active');
-		}
-});
-
-window.addEventListener('keydown', event => {
-
-		if (sidebarBox.classList.contains('active') && event.keyCode === 27) {
-            sidebarBtn.classList.remove('active');
-            sidebarBox.classList.remove('active');
-		}
-});
 
 function App() {
   return (
